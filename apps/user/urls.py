1f99feaba8d4dec2ffe5712340053f641bfa99d1
Andrii-77 <andrijsmaga@gmail.com>
2025-05-31 11:47:48 +0300

    20250531 По lesson5-hw зробив дозвіл адміну блокувати і розблоковувати аккаунт користувача і робити його адміном або блокувати адміна.

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import UserListCreateView
+from .views import BlockUserView, UnBlockUserView, UserBlockAdminView, UserListCreateView, UserToAdminView
 
 urlpatterns = [
-    path('', UserListCreateView.as_view(), name='user_list_create')
+    path('', UserListCreateView.as_view(), name='user_list_create'),
+    path('/<int:pk>/block', BlockUserView.as_view(), name='user_block'),
+    path('/<int:pk>/unblock', UnBlockUserView.as_view(), name='user_unblock'),
+    path('/<int:pk>/to_admin', UserToAdminView.as_view(), name='user_to_admin'),
+    path('/<int:pk>/block_admin', UserBlockAdminView.as_view(), name='block_admin'),
 ]
