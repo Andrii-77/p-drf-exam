fd18c4b02046d87087ce2e56e2de57dfc39a5858
Andrii-77 <andrijsmaga@gmail.com>
2025-08-04 19:17:56 +0300

    20250804 Зробив рефакторинг повторюваного коду в car/serializers.py

--- backend/apps/car/serializers.py
+++ backend/apps/car/serializers.py
@@ -1,10 +1,8 @@
 import logging
-from decimal import InvalidOperation
 
 from rest_framework import serializers
 
 from core.services.banned_words_service import contains_bad_words
-from core.services.currency_conversion_service import CurrencyConverter
 from core.services.currency_conversion_utils import apply_currency_conversion
 from core.services.email_service import EmailService
 
@@ -40,15 +38,14 @@ class CarPosterSerializer(serializers.ModelSerializer):
     daily_views = serializers.SerializerMethodField()
     weekly_views = serializers.SerializerMethodField()
     monthly_views = serializers.SerializerMethodField()
+    stats_message = serializers.SerializerMethodField()
 
     class Meta:
         model = CarPosterModel
-        # fields = ('id', 'brand', 'model', 'description', 'price', 'currency', 'location', 'status', 'edit_attempts',
-        #           'updated_at', 'created_at')
         fields = ('id', 'brand', 'model', 'description', 'original_price', 'original_currency', 'price_usd',
                   'price_eur', 'price_uah', 'exchange_rate_used', 'location', 'status', 'edit_attempts',
                   'region_average_price', 'country_average_price', 'total_views', 'daily_views', 'weekly_views',
-                  'monthly_views', 'updated_at', 'created_at')
+                  'monthly_views', 'stats_message', 'updated_at', 'created_at')
         # read_only_fields = ['status', 'edit_attempts']  # щоб не підміняли вручну
 
     def validate_original_price(self, original_price):
@@ -60,114 +57,53 @@ class CarPosterSerializer(serializers.ModelSerializer):
         price = attrs.get('original_price')
         currency = attrs.get('original_currency')
 
-        # if not price or not currency:
-        #     raise serializers.ValidationError("Ціна та валюта обов’язкові.")
-        # Це не потрібне, бо перевірка наявності цих полів виконується в моделі.
-
         if not price:
             raise serializers.ValidationError("Щоб змінити валюту, також передайте ціну.")
         if not currency:
             raise serializers.ValidationError("Щоб змінити ціну, також передайте валюту.")
 
-        #     rates = self.get_latest_exchange_rates()
-        #
-        #     if currency not in rates and currency != 'UAH':
-        #         raise serializers.ValidationError(f"Немає курсу для валюти {currency}")
-        #
-        #     try:
-        #         if currency == 'UAH':
-        #             price_uah = price
-        #         else:
-        #             price_uah = (price * rates[currency]).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
-        #
-        #         # Конвертація в USD і EUR — незалежно від валюти
-        #         price_usd = (price_uah / rates['USD']).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
-        #         price_eur = (price_uah / rates['EUR']).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
-        #
-        #         attrs['price_uah'] = price_uah
-        #         attrs['price_usd'] = price_usd
-        #         attrs['price_eur'] = price_eur
-        #         attrs['exchange_rate_used'] = {k: str(v) for k, v in rates.items()}
-        #
-        #     except (InvalidOperation, KeyError, TypeError, ValueError) as e:
-        #         logger.exception("Помилка при конвертації валюти:")
-        #         raise serializers.ValidationError(f"Помилка при конвертації валюти: {e}")
-        #
-        #     return attrs
-        #
-        # def get_latest_exchange_rates(self):
-        #     latest_date = ExchangeRate.objects.aggregate(latest=Max("date"))["latest"]
-        #     if not latest_date:
-        #         raise serializers.ValidationError("У базі немає записів із курсами валют.")
-        #
-        #     rates = ExchangeRate.objects.filter(date=latest_date)
-        #     rates_dict = {}
-        #
-        #     for rate in rates:
-        #         try:
-        #             rates_dict[rate.currency] = Decimal(str(rate.rate))
-        #         except (InvalidOperation, TypeError, ValueError) as e:
-        #             raise serializers.ValidationError(f"Невірне значення курсу для {rate.currency}: {e}")
-        #
-        #     # Перевірка обов’язкових валют
-        #     if 'USD' not in rates_dict or 'EUR' not in rates_dict:
-        #         raise serializers.ValidationError("Не знайдено всіх обов’язкових курсів (USD, EUR).")
-        #
-        #     return rates_dict
-
-        # try:
-        #     rates = CurrencyConverter.get_latest_rates()
-        #     converted = CurrencyConverter.convert_price(price, currency, rates)
-        #
-        #     attrs.update(converted)
-        # except (InvalidOperation, KeyError, TypeError, ValueError) as e:
-        #     logger.exception("Помилка при конвертації валюти:")
-        #     raise serializers.ValidationError(f"Помилка при конвертації валюти: {e}")
-        #
-        # return attrs
-
         converted = apply_currency_conversion(price, currency)
         attrs.update(converted)
         return attrs
-
-        # rates = CurrencyConverter.get_latest_rates()
-        # converted = CurrencyConverter.convert_price(price, currency, rates)
-        # attrs.update(converted)
 
     def _can_view_stats(self, obj):
         user = self.context['request'].user
         return has_premium_access(user, obj.user)
 
+    def get_stats_message(self, obj):
+        if not self._can_view_stats(obj):
+            return "Статистика доступна тільки для преміум-продавця власника оголошення, менеджера або адміністратора"
+        return None
+
     def get_region_average_price(self, obj):
         if self._can_view_stats(obj):
             return get_average_prices(obj)['region_average_price']
-        return "Ця інформація доступна тільки для преміум-продавця власника оголошення, менеджера або адміністратора"
+        return None
 
     def get_country_average_price(self, obj):
         if self._can_view_stats(obj):
             return get_average_prices(obj)['region_average_price']
-        return "Ця інформація доступна тільки для преміум-продавця власника оголошення, менеджера або адміністратора"
+        return None
 
     def get_total_views(self, obj):
         if self._can_view_stats(obj):
             return get_view_counts(obj)['total_views']
-        return "Ця інформація доступна тільки для преміум-продавця власника оголошення, менеджера або адміністратора”"
+        return None
 
     def get_daily_views(self, obj):
         if self._can_view_stats(obj):
             return get_view_counts(obj)['daily_views']
-        return "Ця інформація доступна тільки для преміум-продавця власника оголошення, менеджера або адміністратора”"
+        return None
 
     def get_weekly_views(self, obj):
         if self._can_view_stats(obj):
             return get_view_counts(obj)['weekly_views']
-        return "Ця інформація доступна тільки для преміум-продавця власника оголошення, менеджера або адміністратора”"
+        return None
 
     def get_monthly_views(self, obj):
         if self._can_view_stats(obj):
             return get_view_counts(obj)['monthly_views']
-        return "Ця інформація доступна тільки для преміум-продавця власника оголошення, менеджера або адміністратора”"
-
+        return None
 
     def create(self, validated_data):
         description = validated_data.get('description', '')
@@ -203,50 +139,7 @@ class CarPosterSerializer(serializers.ModelSerializer):
 
         instance.description = description
 
-        # # Якщо змінено тільки одне з полів — повертаємо помилку
-        # if 'original_price' in validated_data and 'original_currency' not in validated_data:
-        #     raise serializers.ValidationError("Щоб змінити ціну, також передайте валюту.")
-        # if 'original_currency' in validated_data and 'original_price' not in validated_data:
-        #     raise serializers.ValidationError("Щоб змінити валюту, також передайте ціну.")
-
-        # Якщо змінено ціну і валюту — перерахунок
-
-        # currency = validated_data.get("original_currency", instance.original_currency)
-        #             # price = validated_data.get("original_price", instance.original_price)
-        #             # rates = self.get_latest_exchange_rates()
-        #             #
-        #             # # ❗️Фікс — перетворення Decimal → str
-        #             # instance.exchange_rate_used = {k: str(v) for k, v in rates.items()}
-        #             #
-        #             # if currency == 'USD':
-        #             #     instance.price_usd = price
-        #             #     instance.price_uah = round(price * rates['USD'], 2)
-        #             #     instance.price_eur = round((price * rates['USD']) / rates['EUR'], 2)
-        #             # elif currency == 'EUR':
-        #             #     instance.price_eur = price
-        #             #     instance.price_uah = round(price * rates['EUR'], 2)
-        #             #     instance.price_usd = round((price * rates['EUR']) / rates['USD'], 2)
-        #             # elif currency == 'UAH':
-        #             #     instance.price_uah = price
-        #             #     instance.price_usd = round(price / rates['USD'], 2)
-        #             #     instance.price_eur = round(price / rates['EUR'], 2)
-
         if 'original_price' in validated_data and 'original_currency' in validated_data:
-            # try:
-            #     rates = CurrencyConverter.get_latest_rates()
-            #     converted = CurrencyConverter.convert_price(
-            #         validated_data['original_price'],
-            #         validated_data['original_currency'],
-            #         rates
-            #     )
-            #     instance.price_usd = converted['price_usd']
-            #     instance.price_eur = converted['price_eur']
-            #     instance.price_uah = converted['price_uah']
-            #     instance.exchange_rate_used = converted['exchange_rate_used']
-            # except Exception as e:
-            #     logger.exception("Помилка при оновленні ціни:")
-            #     raise serializers.ValidationError(f"Помилка при оновленні ціни: {e}")
-
             converted = apply_currency_conversion(
                 validated_data['original_price'],
                 validated_data['original_currency']
@@ -264,6 +157,3 @@ class BannedWordsSerializer(serializers.ModelSerializer):
     class Meta:
         model = BannedWordsModel
         fields = ('id', 'word', 'updated_at', 'created_at')
-
-
-ields = ('id', 'word', 'updated_at', 'created_at')
